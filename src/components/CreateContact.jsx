3d91c267beac125f0a55f4af655e19a510b69382
Nicolas Hery <hery.nicolas@gmail.com>
2015-02-16 13:27:19 -0500

    Upgrade to react-router 0.12

--- src/components/CreateContact.jsx
+++ src/components/CreateContact.jsx
@@ -1,16 +1,13 @@
 var React = require('react');
-var whenNode = require('react-router/node_modules/when/node');
 var createContact = require('../actions/createContact');
 
 var CreateContact = React.createClass({
   statics: {
-    willTransitionTo: function(transition, params, query) {
-      transition.wait(whenNode.call(function(cb) {
-        transition.context.executeAction(createContact, query, function() {
-          transition.redirect('/contacts');
-          cb();
-        });
-      }));
+    willTransitionTo: function(transition, params, query, cb) {
+      transition.context.executeAction(createContact, query, function() {
+        transition.redirect('/contacts');
+        cb();
+      });
     }
   },
 
